9038c8e23fe6dc00c3c93798bf90133086026394
Shreya2699 <64342572+Shreya2699@users.noreply.github.com>
2020-04-28 13:32:19 -0700

    Create q2.py
    task1 question2

---
+++ q2.py
@@ -0,0 +1,13 @@
+a=int(input("enter a positive integer "))
+sum=0
+d=a
+if a<=0:
+    print("enter a valid positive integer")
+else :
+    while a>0 :
+        sum=sum+a
+        a=a-1
+    print("sum of the first", d ,"natural number is",sum)
+
+
+
